7488499e45c94d2d78f6b612835d5b3dee8a9257
Armin Kuster <akuster808@gmail.com>
2020-07-15 20:21:13 -0700

    ci-build: clean up old caches
    Signed-off-by: Armin Kuster <akuster808@gmail.com>

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -9,4 +9,8 @@ IMAGES=$@
 ( kas shell kas/${CONF}.yml -c "bitbake --setscene-only ${IMAGES}" || true ) | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
 kas shell kas/${CONF}.yml -c "bitbake --skip-setscene ${IMAGES}" | sed -e '/^NOTE: .*Started$/d' -e '/^NOTE: Running /d'
 
+export SSTATE_CACHE_DIR=/home/srv/sstate/master
+
+./poky/scripts/sstate-cache-management.sh -d -y
+
 mv build/tmp/deploy/images .
